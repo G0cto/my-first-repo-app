d0006def68c9f987a37e1ecb2df428b12c09090f
nawew_e <nawew_e@f213.ub.lan>
2026-09-24 08:49:57 +0800

    feat: update app title with github greeting

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('my-first-repo-app');
+  title = 'Hello GitHub! This is my first Angular push'
 }
