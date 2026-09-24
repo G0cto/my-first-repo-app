0f1e908abf7190af60ea5310c2fd24ee7649d77e
G0cto <enriquegiovann02@gmail.com>
2026-09-24 10:54:19 +0800

    feat: added css

--- src/app/trainer-service.ts
+++ src/app/trainer-service.ts
@@ -1,36 +1,51 @@
 import { Injectable } from '@angular/core';
 
+export interface HoennGymLeader {
+  name: string;
+  team: string[];
+  items: string[];
+  image: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class TrainerService {
 
-  gymLeaders = [
+  gymLeaders: HoennGymLeader[] = [
     {
       name: 'Roxanne',
       team: ['Geodude', 'Nosepass'],
-      items: ['Potion']
+      items: ['Potion'],
+      image: 'https://archives.bulbagarden.net/wiki/Special:Redirect/file/Spr_RS_Roxanne.png'
     },
+
     {
       name: 'Brawly',
       team: ['Machop', 'Makuhita'],
-      items: ['Potion']
+      items: ['Potion'],
+      image: 'https://archives.bulbagarden.net/wiki/Special:Redirect/file/Spr_RS_Brawly.png'
     },
+
     {
       name: 'Wattson',
       team: ['Magnemite', 'Voltorb', 'Magneton'],
-      items: ['Potion']
+      items: ['Potion'],
+      image: 'https://archives.bulbagarden.net/wiki/Special:Redirect/file/Spr_RS_Wattson.png'
     },
+
     {
       name: 'Flannery',
       team: ['Slugma', 'Numel', 'Torkoal'],
-      items: ['Hyper Potion']
+      items: ['Potion'],
+      image: 'https://archives.bulbagarden.net/wiki/Special:Redirect/file/Spr_RS_Flannery.png'
     },
+
     {
       name: 'Norman',
       team: ['Slaking', 'Vigoroth', 'Linoone'],
-      items: ['Hyper Potion']
+      items: ['Potion'],
+      image: 'https://archives.bulbagarden.net/wiki/Special:Redirect/file/Spr_RS_Norman.png'
     }
   ];
-
 }
